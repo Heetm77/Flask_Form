7dff79dce4c849166cd039b994912f3c032ac1e8
Heet <heetmehta2611@gmail.com>
2025-04-15 23:16:40 +0530

    Day 51: Showing submission notification

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 
 # created app instance using the Flask class
@@ -33,6 +33,7 @@ def index():
         form = Form(first_name=first_name, last_name=last_name, email=email, date=date_obj, occupation=occupation)
         db.session.add(form)
         db.session.commit()
+        flash(f"{first_name}, your form was submitted successfully!", "success")
 
     return render_template("index.html")
 
